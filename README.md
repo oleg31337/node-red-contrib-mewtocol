845868ddec92065989ae8c3d89203f7ed6ff9a7f
Oleg <oleg31337@yahoo.com>
2022-10-27 20:13:27 -0400

    Adding RK and RS nodes changing version to 0.0.5

--- README.md
+++ README.md
@@ -4,13 +4,15 @@
 Please use official MEWTOCOL manual as a reference: [MEWTOCOL.pdf](https://mediap.industry.panasonic.eu/assets/custom-upload/Factory%20&%20Automation/PLC/Manuals/mn_all_plcs_mewtocol_user_pidsx_en.pdf)
 
 At the moment only few read nodes are implemented:
-- RD - Read Registers
+- RD - Read Registers as 16bit values
 - RCS - Read single Contact binary value
-- RCC - Read Contact values in 16bit integers
+- RCC - Read Contact values as 16bit integers
 - RT - Read PLC status
-- RR - Read system register
+- RR - Read System Register values as 16bit integers
+- RS - Read Timer Set values as 16bit integers
+- RK - Read Timer Elapsed values as 16bit integers
 
-... more are coming soon ...
+... more are coming ...
 
 ### Install
 
---
+++ mewtocol/mewtocol-rk.js
@@ -0,0 +1,34 @@
+const MewClient = require('jsmewtocol');
+module.exports = function(RED) {
+    function MewtocolRK(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            node.server = RED.nodes.getNode(config.server);
+            var mewclient = new MewClient(node.server.host, node.server.port, node.server.timeout);
+            node.status({fill:"green",shape:"dot",text:"connected"});
+            var station = msg.station?msg.station:config.station;
+            var startaddress=msg.startaddress?msg.startaddress:config.startaddress;
+            var endaddress=msg.endaddress?msg.endaddress:config.endaddress;
+            if (!(station && startaddress && endaddress)) {
+                node.status({fill:"red",shape:"dot",text:"missing parameters"});
+                node.error((config.name||"mewtocol-rk")+": Missing parameters");
+                return;
+            }
+            mewclient.RR(station,startaddress,endaddress)
+            .then(function(data){
+                msg.payload=data;
+                node.send([msg,null]);
+                node.status({fill:"grey",shape:"ring",text:"disconnected"});
+                return mewclient.destroy();
+            })
+            .catch(function(err){
+                node.send([null,err]);
+                node.status({fill:"red",shape:"dot",text:"error"});
+                node.error(err);
+                return mewclient.destroy();
+            });
+        });
+    }
+    RED.nodes.registerType("mewtocol-rk",MewtocolRK);
+}
